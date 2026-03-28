81858f63d95a2f861509e4a3a59b56c9a275a5e7
DevHotchandani <hotchandanidev@gmail.com>
2026-03-29 00:15:23 +0530

    Time: 9 ms (45.71%), Space: 178.9 MB (8.57%) - LeetHub

---
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+
+        for (int i = 0; i < n; i++) {
+            if (lcp[i][i] != n - i) return "";
+            for (int j = 0; j < n; j++) {
+                if (lcp[i][j] != lcp[j][i]) return "";
+            }
+        }
+
+        char[] res = new char[n];
+        int nextChar = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (res[i] != 0) continue;
+
+            if (nextChar >= 26) return "";
+            char c = (char) ('a' + nextChar++);
+
+            for (int j = i; j < n; j++) {
+                if (lcp[i][j] > 0) {
+                    res[j] = c;
+                }
+            }
+        }
+
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (res[i] == res[j]) {
+                    if (i + 1 < n && j + 1 < n)
+                        dp[i][j] = 1 + dp[i + 1][j + 1];
+                    else
+                        dp[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (dp[i][j] != lcp[i][j]) return "";
+
+        return new String(res);
+    }
+}

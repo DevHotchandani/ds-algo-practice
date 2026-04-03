3ca067729ff8d2d6fc04ee86e81763c5375de323
DevHotchandani <hotchandanidev@gmail.com>
2026-04-04 01:12:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (68.52%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            n--;
+            int r = n % 26;
+            sb.append((char) ('A' + r));
+            n /= 26;
+        }
+
+        return sb.reverse().toString();
+    }
+}

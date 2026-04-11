7c5abba7a38b4c3594f6e777b0f5c196af9623f2
DevHotchandani <hotchandanidev@gmail.com>
2026-04-11 14:53:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (90.86%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int integerReplacement(int n) {
+        long x = n;
+        int steps = 0;
+
+        while (x != 1) {
+            if ((x & 1) == 0) {
+                x >>= 1;
+            } else {
+                if (x == 3 || (x & 3) == 1) x--;
+                else x++;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+}
